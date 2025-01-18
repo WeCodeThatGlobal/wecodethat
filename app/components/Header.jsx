dc986c1aefb68595e82dacf94428a026012d08a6
Karthik <rajakarthik131@gmail.com>
2025-01-18 15:06:03 +0530

    Add contact page

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,11 +4,11 @@ import "./Header.css";
 const Heading = ({ title, subtitle, alignment = "text-center", titleCss }) => {
   return (
     <div>
-      <div className={`section-title ${alignment}`}>
-        <span className="text-blue-600 subtitle mb-2 pb-3 relative uppercase font-bold leading-relaxed">
+      <div className={`section-title text-3xl md:text-4xl ${alignment}`}>
+        <span className="text-blue-600 text-base subtitle mb-2 pb-3 relative uppercase font-bold leading-relaxed">
           {subtitle}
         </span>
-        <h2 className={`leading-tight text-3xl md:text-4xl mt-6 font-bold ${titleCss}`}>{title}</h2>
+        <h2 className={`leading-tight mt-6 font-bold ${titleCss}`}>{title}</h2>
       </div>
     </div>
   );
---
+++ app/contact-us/page.jsx
@@ -0,0 +1,211 @@
+"use client";
+import React from "react";
+import { Navbar } from "../components/Navbar";
+import Header from "../components/Header";
+import { useRef, useEffect, useState } from "react";
+import useScrollPosition from "../hooks/useScrollPosition";
+
+import FooterBlock from "../components/Footer";
+import PageHeader from "../components/PageHeader";
+import ScrollToTop from "../components/ScrollToTop";
+
+const page = () => {
+  const box = useRef(null);
+  const scrollPosition = useScrollPosition();
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+
+  useEffect(() => {
+    const updateScrollPercentage = () => {
+      const scrollHeight = document.body.scrollHeight - window.innerHeight;
+      const percentage =
+        scrollHeight > 0 ? (scrollPosition / scrollHeight) * 100 : 0;
+      setScrollPercentage(percentage);
+    };
+
+    updateScrollPercentage();
+  }, [scrollPosition]);
+
+
+	// The code below will load the embed
+	useEffect(() => {
+		const widgetScriptSrc = 'https://tally.so/widgets/embed.js';
+
+    const load = () => {
+      // Load Tally embeds
+      if (typeof Tally !== 'undefined') {
+        Tally.loadEmbeds();
+        return;
+      }
+
+      // Fallback if window.Tally is not available
+      document
+        .querySelectorAll('iframe[data-tally-src]:not([src])')
+        .forEach((iframeEl) => {
+          iframeEl.src = iframeEl.dataset.tallySrc;
+        });
+    };
+
+    // If Tally is already loaded, load the embeds
+    if (typeof Tally !== 'undefined') {
+      load();
+      return;
+    }
+
+    // If the Tally widget script is not loaded yet, load it
+    if (document.querySelector(`script[src="${widgetScriptSrc}"]`) === null) {
+      const script = document.createElement('script');
+      script.src = widgetScriptSrc;
+      script.onload = load;
+      script.onerror = load;
+      document.body.appendChild(script);
+      return;
+    }
+	}, []);
+
+  return (
+    <div ref={box}>
+      <Navbar />
+      <PageHeader title="Contact Us" />
+      <div className="container mx-auto px-4 md:px-8 my-12">
+        <div className="max-w-7xl ">
+          <div className="bg-slate-100 px-8 py-6">
+            <div className="grid grid-cols-3 gap-4">
+              <div className="col-span-2">
+                <h2 className="font-bold text-3xl mb-4">Write Us Something </h2>
+
+                <div>
+                  <iframe data-tally-src="https://tally.so/embed/wvE690?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="276" frameborder="0" marginheight="0" marginwidth="0" title="Contact form"></iframe>
+                </div>
+              </div>
+              <div className="col-span-1">
+                <div className="bg-gray-900 px-6 py-4 rounded-md">
+                  <Header
+                    alignment="text-left"
+                    title="Our Contact Details"
+                    subtitle="Who we are"
+                    titleCss="text-white text-2xl"
+                  />
+
+                  <div className="divide-y divide-gray-600 mt-8">
+                    <div className="flex items-center group py-4">
+                      <div className="bg-slate-600 rounded-md text-white p-4 group-hover:bg-blue-500">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="size-6"
+                        >
+                          <path d="M19.5 22.5a3 3 0 0 0 3-3v-8.174l-6.879 4.022 3.485 1.876a.75.75 0 1 1-.712 1.321l-5.683-3.06a1.5 1.5 0 0 0-1.422 0l-5.683 3.06a.75.75 0 0 1-.712-1.32l3.485-1.877L1.5 11.326V19.5a3 3 0 0 0 3 3h15Z" />
+                          <path d="M1.5 9.589v-.745a3 3 0 0 1 1.578-2.642l7.5-4.038a3 3 0 0 1 2.844 0l7.5 4.038A3 3 0 0 1 22.5 8.844v.745l-8.426 4.926-.652-.351a3 3 0 0 0-2.844 0l-.652.351L1.5 9.589Z" />
+                        </svg>
+                      </div>
+                      <div className="ml-4">
+                        <h4 className="text-base font-bold text-white">
+                          Send E-Mail
+                        </h4>
+                        <p className="text-gray-400">info@example.com</p>
+                      </div>
+                    </div>
+                    <div className="flex items-center group py-4">
+                      <div className="bg-slate-600 rounded-md text-white p-4 group-hover:bg-blue-500">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                      <div className="ml-4">
+                        <h4 className="text-base font-bold text-white">
+                          Call Anytime
+                        </h4>
+                        <p className="text-gray-400">info@example.com</p>
+                      </div>
+                    </div>
+                    <div className="flex items-center group py-4">
+                      <div className="bg-slate-600 rounded-md text-white p-4 group-hover:bg-blue-500">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                      <div className="ml-4">
+                        <h4 className="text-base font-bold text-white">
+                          Locations
+                        </h4>
+                        <p className="text-gray-400">India</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="flex mt-4 text-white w-full gap-3">
+                      <a href="#" className="bg-slate-600 rounded-md text-white p-2" aria-label="social link" rel="noreferer">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          className="w-5 h-5"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
+                        </svg>
+                      </a>
+                      <a href="#" className="bg-slate-600 rounded-md text-white p-2"aria-label="social link" rel="noreferer">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          className="w-5 h-5"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"></path>
+                        </svg>
+                      </a>
+                      <a href="#" className="bg-slate-600 rounded-md text-white p-2"aria-label="social link" rel="noreferer">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          className="w-5 h-5"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"></path>
+                        </svg>
+                      </a>
+                      <a href="#" className="bg-slate-600 rounded-md text-white p-2"aria-label="social link" rel="noreferer">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          className="w-5 h-5"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <FooterBlock />
+      <ScrollToTop percentage={scrollPercentage} />
+    </div>
+  );
+};
+
+export default page;
