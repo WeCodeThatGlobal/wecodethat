2eb5af91ec53e7be63306830613cdeb9ac8b9b80
sasidaranedu <sasidaranasokan@gmail.com>
2025-01-23 19:18:44 +0530

    feat: add content to homepage

--- app/components/WhoWeAre2.jsx
+++ app/components/WhoWeAre2.jsx
@@ -13,17 +13,70 @@ import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/react/24/outline";
 const faqs = [
   {
     id: "1",
-    question: "What's the best thing about Switzerland?",
-    answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+    question: "Why Choose WeCodeThat?",
+    para_1:
+      "WeCodeThat is your partner in transforming ideas into reality. With a team of experienced developers, designers, and strategists, we prioritize delivering high-quality solutions tailored to your unique needs. Whether you're a startup or an established business, we bring your vision to life with speed, precision, and creativity.",
+    answer: [],
+    para_2: "",
   },
   {
     id: "2",
-    question: "What's the best thing about Switzerland?",
-    answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+    question: "How Can We Help You?",
+    para_1: "We specialize in:",
+    answer: [
+      "Custom Web and Mobile App Development",
+      "UI/UX Design to create stunning user experiences",
+      "MVP Development for startups to launch faster",
+      "E-commerce Solutions to boost your online sales",
+      "Ongoing Maintenance and Support for your projects",
+    ],
+    para_2: "From ideation to execution, we’re here to make it happen.",
   },
-  // More questions...
+  {
+    id: "3",
+    question: "What Makes Us Different?",
+
+    answer: [
+      <>
+        <strong>Collaborative Approach:</strong> We work with you every step of
+        the way.
+      </>,
+      <>
+        <strong>End-to-End Services:</strong> From strategy to deployment, we
+        cover it all.
+      </>,
+      <>
+        <strong>Scalable Solutions:</strong> Our solutions grow with your
+        business.
+      </>,
+      <>
+        <strong>Commitment to Excellence:</strong> Quality is at the heart of
+        what we do.
+      </>,
+    ],
+  },
+  {
+    id: "4",
+    question: "How Do We Work?",
+    answer: [
+      <>
+        <strong>Understanding Your Needs:</strong> We start by discussing your
+        goals and challenges.
+      </>,
+      <>
+        <strong>Strategic Planning:</strong> Our experts create a roadmap
+        tailored to your project.
+      </>,
+      <>
+        <strong>Seamless Execution:</strong>Using agile methodologies, we build,
+        test, and refine.
+      </>,
+      <>
+        <strong>Ongoing Support:</strong> We ensure everything runs smoothly
+        post-launch.
+      </>,
+    ],
+  },
 ];
 
 const FAQ = () => {
@@ -55,8 +108,16 @@ const FAQ = () => {
                     </span>
                   </DisclosureButton>
                 </dt>
-                <DisclosurePanel as="dd" className="mt-2 pr-12">
-                  <p className="text-base/7 text-gray-600">{faq.answer}</p>
+                <DisclosurePanel as="dd" className="mt-2 pr-12 space-y-4">
+                  <p className="text-base/7 text-gray-600">{faq.para_1}</p>
+                  <ul className="list-disc ml-10 space-y-2">
+                    {faq.answer.map((item) => (
+                      <li key={faq.id} className="text-base/7 text-gray-600">
+                        {item}
+                      </li>
+                    ))}
+                  </ul>
+                  <p className="text-base/7 text-gray-600">{faq.para_2}</p>
                 </DisclosurePanel>
               </Disclosure>
             ))}
